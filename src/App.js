75680ea25ea1bc5bc83259736442ede0f5e9526f
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-10-26 17:37:52 +0900

    feat: カウントをミリ秒、秒、分で表示する

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 import MeasurementButton from "./components/MeasurementButton";
+import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0);
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
+  const [milliseconds, setMilliseconds] = useState("000");
+
   const [stopWatchId, setStopWatchId] = useState(null);
+
   const [isStartActive, setIsStartActive] = useState(true);
   const [isStopActive, setIsStopActive] = useState(false);
   const [isResetActive, setIsResetActive] = useState(false);
 
+  let startTime;
+
   const countUp = () => {
-    setTime((time) => time + 1);
+    const d = new Date(Date.now() - startTime);
+
+    const upDateMinutes = String(d.getMinutes()).padStart(2, "0");
+    const upDateSeconds = String(d.getSeconds()).padStart(2, "0");
+    const upDateMilliseconds = String(d.getMilliseconds()).padStart(3, "0");
+
+    setMinutes(upDateMinutes);
+    setSeconds(upDateSeconds);
+    setMilliseconds(upDateMilliseconds);
   };
 
-  const startClick = () => {
+  const stopWatchClick = () => {
     if (stopWatchId === null) {
-      setStopWatchId(setInterval(countUp, 1000));
+      startTime = new Date();
+      setStopWatchId(setInterval(countUp, 33));
       setIsStartActive(false);
       setIsStopActive(true);
       setIsResetActive(false);
-    }
-  };
-
-  const stopClick = () => {
-    if (stopWatchId !== null) {
+    } else {
       clearInterval(stopWatchId);
       setStopWatchId(null);
       setIsStartActive(true);
@@ -32,27 +44,32 @@ function App() {
   };
 
   const resetClick = () => {
-    setTime(0);
+    setMinutes("00");
+    setSeconds("00");
+    setMilliseconds("000");
   };
 
   return (
     <div className="App">
-      <div>
-        <p>{time}</p>
+      <div className="App__head">
+        <p>
+          <span>{minutes}</span>:<span>{seconds}</span>:
+          <span>{milliseconds}</span>
+        </p>
       </div>
-      <div>
+      <div className="App__body">
         <MeasurementButton
           onClick={resetClick}
           value={"Reset"}
           isActive={isResetActive}
         />
         <MeasurementButton
-          onClick={startClick}
+          onClick={stopWatchClick}
           value={"Start"}
           isActive={isStartActive}
         />
         <MeasurementButton
-          onClick={stopClick}
+          onClick={stopWatchClick}
           value={"Stop"}
           isActive={isStopActive}
         />
